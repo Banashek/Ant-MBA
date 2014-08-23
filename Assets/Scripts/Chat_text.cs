af2b570526698f4a9d31cb042d316b0a8890b8dc
Jake Cohen <Jake@Jakes-MacBook-Air.local>
2014-08-23 02:52:56 -0700

    dialogue implemented

--- Assets/Scripts/Chat_text.cs
+++ Assets/Scripts/Chat_text.cs
@@ -4,24 +4,34 @@ using SimpleJSON;
 
 public class Chat_text : MonoBehaviour {
 	public JSONNode chat_node;
-	public string chat_json = 
-		"{" +
-			"'scene':1," +
-				"'chat':[" +
-					"{" +
-						"'person':'Susan R. Bumpershoot'," +
-						"'wait':3," +
-						"'say':'Everyone please welcome the ant to the business.'" +
-					"}," +
-					"{" +
-						"'person':'Nick K. Nuckle'," +
-						"'wait':3," +
-						"'say':'What's his name?'" +
-					"}" +
-				"]" +
-			"}" +
-		"}";
-
+	public string chat_json = "{" +
+								"\"dialogue1\":" +
+									"[" +
+										"{" +
+											"\"person\":\"Susan R. Bumpershoot\"," +
+											"\"wait\":3," +
+											"\"say\":\"Everyone, please welcome the ant to the company.\"" +
+										"}," +
+										"{" +
+											"\"person\":\"Nick K. Nuckle\"," +
+											"\"wait\":3," +
+											"\"say\":\"What is his name?\"" +
+										"}" +
+									"]," +
+								"\"dialogue2\":" +
+									"[" +
+										"{" +
+											"\"person\":\"Illiterate Bill\"," +
+											"\"wait\":3," +
+											"\"say\":\"What did he type?\"" +
+										"}," +
+										"{" +
+											"\"person\":\"You\"," +
+											"\"wait\":1," +
+											"\"say\":\".......\"" +
+									"]" +
+								"}";
+	
 	// Use this for initialization
 	void Awake () {
 		chat_node = JSON.Parse (chat_json);
