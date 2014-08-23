6b3bb8dc60c089c7b2f4609b6328944328d034eb
Jake Cohen <Jake@Jakes-MacBook-Air.local>
2014-08-22 23:36:13 -0700

    begun chat workflow

---
+++ Assets/Scripts/Chat_text.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public class Chat_text : MonoBehaviour {
+	public JSONNode chat_node;
+	public string chat_json = 
+		"{" +
+			"'scene':1," +
+				"'chat':[" +
+					"{" +
+						"'person':'Susan R. Bumpershoot'," +
+						"'wait':3," +
+						"'say':'Everyone please welcome the ant to the business.'" +
+					"}," +
+					"{" +
+						"'person':'Nick K. Nuckle'," +
+						"'wait':3," +
+						"'say':'What's his name?'" +
+					"}" +
+				"]" +
+			"}" +
+		"}";
+
+	// Use this for initialization
+	void Awake () {
+		chat_node = JSON.Parse (chat_json);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Dialogue_controller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+
+public class Dialogue_controller : MonoBehaviour {
+	private JSONNode chat_node;
+	private float total_time = 0f;
+	// Use this for initialization
+	void Start () {
+		chat_node = GameObject.FindGameObjectWithTag ("json").GetComponent<Chat_text>().chat_node;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (total_time > 1) {
+			Debug.Log (chat_node[0][0]);
+			total_time = -10;
+		} else if (total_time >= 0) {
+			total_time += Time.deltaTime;
+		}
+	}
+}
