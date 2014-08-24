8063036d0f5a7b5a7cfbb8da52a5e4e822267186
Jake Cohen <Jake@Jakes-MacBook-Air.local>
2014-08-23 18:31:20 -0700

    Shadows and stomp generation implemented. TODO: Add pounding feet, level restart, level end (objects in place)

---
+++ Assets/Scripts/Hallway/Foot_Stomp_Controller.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Foot_Stomp_Controller : MonoBehaviour {
+
+	public GameObject foot_shadow;
+	public float seconds_before_the_pounding = 2f;
+	private Transform ant;
+	private float timer=0f;
+	private float wait_time =.5f;
+	private Vector3 last_stomp_location;
+	private bool stompings_begun = false;
+
+	void Start () {
+		ant = GameObject.FindGameObjectWithTag ("Player").transform;
+		last_stomp_location = ant.position;
+	}
+
+	void Update () {
+		if(stompings_begun){
+			if (timer > wait_time) {
+				//stomp
+				Vector3 stomp_location = ant.position + new Vector3(Random.Range(0f,2f),Random.Range(-1f,0),0);
+				if(Vector3.Distance(stomp_location,last_stomp_location)<.5f)
+					stomp_location = ant.position + new Vector3(Random.Range(0f,2f),Random.Range(-1f,0),0);
+				last_stomp_location = stomp_location;
+				Instantiate(foot_shadow,stomp_location,Quaternion.identity);
+				//reset timer
+				wait_time = Random.Range (.5f, 2f);
+				timer = 0f;
+			}
+		}else if(timer>seconds_before_the_pounding){
+			timer=0f;
+			stompings_begun=true;
+		}
+		timer += Time.deltaTime;
+	}
+}
--- Assets/Scripts/Hallway/Top_down_ant_controller.cs
+++ Assets/Scripts/Hallway/Top_down_ant_controller.cs
@@ -4,11 +4,12 @@ using System.Collections;
 public class Top_down_ant_controller : MonoBehaviour {
 
 	public float speed = 1f;
-
+	private Animator ant_animator;
 	private Transform ant_transform;
 	// Use this for initialization
 	void Start () {
 		ant_transform = transform;
+		ant_animator = transform.GetChild(0).GetComponent<Animator>(); //GetComponent<Animator> ();
 		//ant_transform.Rotate (Vector3.forward * 90,Space.World);
 		//ant_transform.rotation = Quaternion.AngleAxis (90f, Vector3.forward);
 	}
@@ -20,16 +21,23 @@ public class Top_down_ant_controller : MonoBehaviour {
 
 	private void Move(){
 		//allows movement in both up/down and right/left directions at same time, but does not allow up and down movement at the same time
+		bool walking = false;
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			ant_transform.Translate (Vector3.right * speed * Time.deltaTime);
+			walking=true;
 		} else if (Input.GetKey (KeyCode.LeftArrow)) {
 			ant_transform.Translate (-Vector3.right * speed * Time.deltaTime);
+			walking=true;
 		}
 
 		if (Input.GetKey (KeyCode.DownArrow)) {
 			ant_transform.Translate (-Vector3.up * speed * Time.deltaTime);
+			walking=true;
 		}else if (Input.GetKey(KeyCode.UpArrow)){
 			ant_transform.Translate (Vector3.up * speed * Time.deltaTime);
+			walking=true;
 		}
+
+		ant_animator.SetBool("walking",walking);
 	}
 }
