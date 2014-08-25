17020d3ff257db96937a36e27ecfba32b43d21de
Jake Cohen <Jake@Jakes-MacBook-Air.local>
2014-08-24 23:02:18 -0700

    Added "Thanks for Playing!" to closing scene

--- Assets/Scripts/Cutscenes/Switch_Backgrounds.cs
+++ Assets/Scripts/Cutscenes/Switch_Backgrounds.cs
@@ -3,8 +3,10 @@ using System.Collections;
 
 public class Switch_Backgrounds : MonoBehaviour {
 	public Sprite new_background;
+	public GUIStyle style;
 	private GameObject bill_dialogue_closing;
 	private bool swap;
+	private bool show_credits;
 
 	void Awake(){
 		bill_dialogue_closing = GameObject.Find ("bill_dialogue_controller");
@@ -16,6 +18,17 @@ public class Switch_Backgrounds : MonoBehaviour {
 			bill_dialogue_closing.GetComponent<Chat_text>().enabled = true;
 			bill_dialogue_closing.GetComponent<Dialogue_controller>().enabled = true;
 			swap = true;
+		}
+
+		if(GameObject.FindGameObjectsWithTag("dialogue").Length==0){
+			show_credits = true;
+		}
+	}
+
+	void OnGUI(){
+		if(show_credits){
+			Debug.Log("Show credits");
+			GUI.Label(new Rect(Screen.width/2-150,Screen.height/2-150,350,350),"Thanks for playing!",style);
 		}
 	}
 }
