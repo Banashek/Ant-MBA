c7709d066180c12713d3ffd94e883175a0d619f9
Jake Cohen <Jake@Jakes-MacBook-Air.local>
2014-08-24 22:04:57 -0700

    prints 4 letters of name to the screen in the conference rooms. fixed hallway2=>closing transition

--- Assets/Scripts/keyboard/Ant_Name.cs
+++ Assets/Scripts/keyboard/Ant_Name.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class Ant_Name : MonoBehaviour {
 	public string name;
 	public int[] cutscenes = new int[3];
-	public Vector3 display_location;
+	public Vector3 display_location,screen_coords;
 	public GUIStyle style;
 	private Camera camera;
+	private bool show_name;
+	private int in_proper_scene = 0;
+
 	// Use this for initialization
 	void Start () {
 		DontDestroyOnLoad (gameObject);
@@ -15,13 +18,23 @@ public class Ant_Name : MonoBehaviour {
 	void Update(){
 		foreach (int scene_number in cutscenes) {
 			if(Application.loadedLevel == scene_number){
+				in_proper_scene +=1;
 				camera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ();
 				Debug.Log("Yeah butty");
-				Vector3 screen_coords = camera.WorldToScreenPoint(display_location);
-				Debug.Log("display_location: "+display_location);
-				Debug.Log("screen_coords: "+screen_coords);
-				GUI.Label(new Rect(screen_coords.x,screen_coords.y,300,1000),name,style);
+				screen_coords = camera.WorldToScreenPoint(display_location);
+				show_name=true;
 			}
+		}
+		if (in_proper_scene == 0){
+			show_name = false;
+			in_proper_scene=0;
+		}
+	}
+
+	void OnGUI(){
+		if(show_name){
+			GUI.Label(new Rect(screen_coords.x,screen_coords.y,300,1000),name,style);
+			Debug.Log ("Showing name");
 		}
 	}
 }
